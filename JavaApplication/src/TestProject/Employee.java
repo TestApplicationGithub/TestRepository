976dcc2f962cdda3f58db5d47772e110c4b02034
KP <589478@CTS.COM>
2016-10-26 11:50:58 +0530

    Java Application Commit

---
+++ JavaApplication/src/TestProject/Employee.java
@@ -0,0 +1,19 @@
+package TestProject;
+
+public class Employee {
+
+	public int age;
+	public String Name;
+	public String Company;
+	
+	public Employee()
+	{
+	}
+	
+	public Employee(int Age, String name, String company){
+	this.age = Age;
+	this.Name = name;
+	this.Company = company;
+		
+	}
+}
---
+++ JavaApplication/src/TestProject/JavaLearning.java
@@ -0,0 +1,15 @@
+package TestProject;
+
+
+public class JavaLearning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee obj = new Employee(25, "Vasanth", "Cognizant");
+		System.out.println("Age - "+obj.age);
+		System.out.println("Name - "+obj.Name);
+		System.out.println("Company - "+obj.Company);
+		
+	}
+
+}
